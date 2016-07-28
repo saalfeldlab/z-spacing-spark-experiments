d09548152be536764cfcc8b4a64069a33b0e9262
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-07-28 12:14:03 -0400

    clean-up of SparkRenderWave

--- src/main/java/org/janelia/thickness/experiments/SparkRenderWave.java
+++ src/main/java/org/janelia/thickness/experiments/SparkRenderWave.java
@@ -1,22 +1,19 @@
 package org.janelia.thickness.experiments;
 
-import ij.ImagePlus;
-import ij.process.FloatProcessor;
-import loci.formats.FileStitcher;
-import loci.formats.FormatException;
+import java.io.IOException;
+import java.util.List;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.PairFunction;
 import org.janelia.thickness.utility.SparkRender;
 import org.janelia.thickness.utility.Utility;
-import scala.Tuple2;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.regex.Pattern;
+import ij.ImagePlus;
+import ij.process.FloatProcessor;
+import loci.formats.FormatException;
+import scala.Tuple2;
 
 /**
  * @author Philipp Hanslovsky &lt;hanslovskyp@janelia.hhmi.org&gt;
@@ -27,9 +24,6 @@ public class SparkRenderWave {
 
         SparkConf sparkConf = new SparkConf().setAppName("Render");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
-
-//        final String sourceFormat = args[0];
-//        final String transformFormat = args[1];
         final String sourceFormat = args[0];
         final String transformFormat = args[1];
         final String outputFormat = args[2];
@@ -38,14 +32,11 @@ public class SparkRenderWave {
         int start = Integer.parseInt(args[5]);
         int stop = Integer.parseInt(args[6]);
 
-
-//        final String outputFormat = args[4];
-
         ImagePlus img0 = new ImagePlus(String.format(sourceFormat, start));
 
         final int width = img0.getWidth();
         final int height = img0.getHeight();
-        final int size = stop - start; // 201;//reader.getSizeZ();
+        final int size = stop - start;
 
         System.out.println( sourceFormat );
         System.out.println( transformFormat );
@@ -69,50 +60,23 @@ public class SparkRenderWave {
 
         JavaRDD<Integer> indices = sc.parallelize(Utility.arange(size));
         JavaPairRDD<Integer, FloatProcessor> sections = indices
-                .mapToPair(new PairFunction<Integer, Integer, Integer>() {
-
-                    public Tuple2<Integer, Integer> call(Integer arg0) throws Exception {
-                        return Utility.tuple2(arg0, arg0);
-                    }
-                })
+                .mapToPair(new Utility.Duplicate<>())
                 .sortByKey()
-                .map(new Function<Tuple2<Integer, Integer>, Integer>() {
-
-                    public Integer call(Tuple2<Integer, Integer> arg0) throws Exception {
-                        return arg0._1();
-                    }
-                })
+                .map(new Utility.DropValue<>())
                 .mapToPair(new Utility.LoadFileFromPattern(sourceFormat))
                 .cache();
         System.out.println( "sections: " + sections.count() );
 
         JavaPairRDD<Integer, FloatProcessor> transforms = indices
-                .mapToPair(new PairFunction<Integer, Integer, Integer>() {
-
-                    public Tuple2<Integer, Integer> call(Integer arg0) throws Exception {
-                        return Utility.tuple2(arg0, arg0);
-                    }
-                })
+                .mapToPair(new Utility.Duplicate<>())
                 .sortByKey()
-                .map(new Function<Tuple2<Integer, Integer>, Integer>() {
-
-                    public Integer call(Tuple2<Integer, Integer> arg0) throws Exception {
-                        return arg0._1();
-                    }
-                })
-                .mapToPair(new PairFunction<Integer, Integer, FloatProcessor>() {
-                    @Override
-                    public Tuple2<Integer, FloatProcessor> call(Integer integer) throws Exception {
-                        ImagePlus imp = new ImagePlus(String.format(transformFormat, integer.intValue()));
-                        FloatProcessor fp = imp.getProcessor().convertToFloatProcessor();
-                        return Utility.tuple2(integer, fp);
-                    }
-                })
+                .map(new Utility.DropValue<>())
+                .mapToPair(new Utility.LoadFileFromPattern( transformFormat ) )
                 .cache();
 
         System.out.println( "transforms: " + transforms.count() );
 
-        JavaPairRDD<Integer, FloatProcessor> transformed = SparkRender.render(sc, sections, transforms, stepsDouble, radiiDouble, dim, 1.0 /* fix this */ ).cache();
+        JavaPairRDD<Integer, FloatProcessor> transformed = SparkRender.render(sc, sections, transforms, stepsDouble, radiiDouble, dim, 1.0 /* fix this */, start ).cache();
         System.out.println( "transformed: " + transformed.count() );
 
 
